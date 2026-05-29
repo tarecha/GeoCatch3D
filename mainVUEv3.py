cc5ed7adbc1ca1949df65e74f76bb84587eec41a
agung <agung.tarecha@gmail.com>
2026-05-30 02:08:18 +0700

    percepatan loop diganti slicing numpy, file temp bisa dicustom taruh di ramdsik dengan mudah ip untuk akses kompatibel windows dan linux

--- mainVUEv3.py
+++ mainVUEv3.py
@@ -8,14 +8,15 @@ import numpy as np
 import pyvista as pv
 from matplotlib.colors import LinearSegmentedColormap
 import matplotlib.pyplot as plt
-import re, subprocess
+import re
 import time
 import webbrowser
 import threading
+import socket
 
 # Modul lokal AGUNG222
 import modul.config as cfg
-from modul import plotter as pla, curahhujan, mapping, seleksiRHD, pilih, visualCallBackTrame, fileHandler, analisis, watershed as wts
+from modul import curahhujan, mapping, seleksiRHD, pilih, visualCallBackTrame, fileHandler, analisis, watershed as wts
 import modul.konverter as knv
 
 # Import Trame UI (Pastikan pakai vuetify3)
@@ -615,7 +616,6 @@ def buka_browser(ip, port=80):
 if __name__ == "__main__":
     print("Akses melalui web browser dari PC lain dengan link berikut:")
 
-    import socket
 
 
     def dapatkan_ip_lokal():
